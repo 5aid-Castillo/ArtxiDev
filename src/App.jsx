05470cff57defbfedaa084f0d9c47da9a2071a4d
Said Castillo <said557@outlook.es>
2024-03-12 00:34:13 -0600

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import { Header } from './components/layouts/header'
+
 import { Home } from './pages/Home'
 import { Services } from './pages/Services'
 import { Footer } from './components/layouts/Footer'
